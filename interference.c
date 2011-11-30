185cd6a7bc417f863b4565b21f4a92bf30361ce0
Chris Jedd <cwjedd@ncsu.edu>
2011-11-30 14:07:26 -0500

    Working Interference Graph Code.  Test Bench file added for testing of interference graph and for future coloring testing.

--- interference.c
+++ interference.c
@@ -15,34 +15,38 @@ intNode* calcInterference(live_range* live, int size)
 	intNode* graph;
 	int* interferences;
 
+	// malloc memory for graph one location for each Register number
 	graph = (intNode*)malloc(size * sizeof(intNode));
 
-	for(i=0;i<size;i++)
+	for(i=0;i<size;i++)								// iterate through every register
 	{
-		degree = 0;
-		interferences = (int*)malloc(5*sizeof(int));
+		degree = 0;								// reset degree for new node
+		interferences = (int*)malloc(5*sizeof(int));		// malloc memory for node's interferences
 		
-		for(j=0;j<size;j++)
+		for(j=0;j<size;j++)							// compare register i with every register
 		{
-			if(i != j)
+			if(i != j)							// do not check node against itself
 			{
-				if(((live[j].go_live >= live[i].go_live) &&
-						 (live[j].go_live <= live[i].dead)) ||
-						((live[j].dead >= live[i].go_live) && 
-						 (live[j].dead <= live[i].dead)))
+				if( ((live[j].go_live >= live[i].go_live) && (live[j].go_live <= live[i].dead)) ||	// check if j's go_live falls within i's live range
+				    ((live[j].dead >= live[i].go_live) && (live[j].dead <= live[i].dead)) ||		// check if j's dead falls within i's live range
+
+				    ((live[i].go_live >= live[j].go_live) && (live[i].go_live <= live[j].dead)) ||	// check if i's go_live falls within j's live range
+				    ((live[i].dead >= live[j].go_live) && (live[i].dead <= live[j].dead)) )		// check if i's dead falls within j's live range
 				{
 					// found that j is an interference to i
+
+					// if interferences array size to small to add more realloc to a larger size
 					if(degree%5 == 0)
 						interferences = (int*)realloc(interferences, (degree+5)*sizeof(int));
 
-					interferences[degree] = j;
+					interferences[degree] = j;		// add register to list of interferences
 					
-					degree++;
+					degree++;						// increment degree of node
 				}
 			}
 		}
-		graph[i].interferences = interferences;
-		graph[i].degree = degree;
+		graph[i].interferences = interferences;			// add interferences to the graph
+		graph[i].degree = degree;					// add degree information to the graph
 	}
-	return graph;
+	return graph;									// return completed graph
 }
